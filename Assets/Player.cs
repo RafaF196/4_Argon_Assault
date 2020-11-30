387e35a928dd9d3140d8fe34a609c17ce358fccb
RafaF196 <RafaF196@gmail.com>
2020-11-30 16:01:25 +0100

    Video 15

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")][SerializeField] float Speed = 10f;
+    [Tooltip("In m")] [SerializeField] float xRange = 4.5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 2.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horitzontal");
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xThrow = Input.GetAxis("Horizontal"), yThrow = Input.GetAxis("Vertical");
+        float xOffset = xThrow * Speed * Time.deltaTime, yOffset = yThrow*Speed*Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset, rawYPos = transform.localPosition.y + yOffset;
+
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
